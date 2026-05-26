4295c605d1209697286fd829d261be96a382ef90
bonelingu <60837581+bonelingu@users.noreply.github.com>
2026-05-26 20:19:49 +0800

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabase-client";
+import { supabase } from "../lib/supabase-client";
 import Graph from "./graph";
 import PersonSearchSelect from "./person-search-select";
 
