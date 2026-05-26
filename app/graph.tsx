46c94398be87d168cd8b95874bb6e950e525f2e8
bonelingu <60837581+bonelingu@users.noreply.github.com>
2026-05-26 19:38:08 +0800

    1

--- app/graph.tsx
+++ app/graph.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import ReactFlow, { Background } from "@xyflow/react";
-import "@xyflow/react/dist/style.css";
+import ReactFlow, { Background } from "reactflow";
+import "reactflow/dist/style.css";
 
 export default function Graph({ people, relations }: any) {
   const nodes = people.map((p: any, i: number) => ({
@@ -21,7 +21,7 @@ export default function Graph({ people, relations }: any) {
   }));
 
   return (
-    <div style={{ height: 500, width: "100%" }}>
+    <div style={{ height: 500 }}>
       <ReactFlow nodes={nodes} edges={edges} fitView>
         <Background />
       </ReactFlow>
